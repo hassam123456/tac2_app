fe90f953ab4b1b63bf2290bf8a5ffcdd1817000b
unknown <hassamahmed873@gmail.com>
2021-09-28 15:46:16 +0500

    this is for

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,48 +1,197 @@
 import 'package:flutter/material.dart';
-
 class homePage extends StatelessWidget {
   const homePage({Key? key}) : super(key: key);
 
-  @override
+  @overridea
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
         title: Text("Tac-Alert"),
         ),
+      body: ListView(
+        children: [
+          Center(
+              child: Column(
+              children: [
+                Container(
+                  child: decorationBox()
+                ),
+                Container(
+                  child: Row(
+                    children:[
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children:[
+                            intRadiationNumbers(1000),
+                            SizedBox(height: 80),
+                            intRadiationNumbers(100),
+                            SizedBox(height: 90),
+                            intRadiationNumbers(5),
+                            SizedBox(height: 90),
+                            doubleRadiationNumbers(0.05),
+                            SizedBox(height: 80),
+                            intRadiationNumbers(0),
+                          ]
+                      ),
+                      Align(
+                          alignment: Alignment(0, 0),
+                          child: radiationLevelBar()
+                      ),
+                      SizedBox(
+                        width: 25.0,
+                      ),
+                      //first box
+                      SizedBox(
+                        height: 80,
+                      ),
+                      Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                child: Row(
+                                  children: [
+                                    SizedBox(
+                                      width: 10.0,
+                                    ),
+                                    Text('Very High',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold
+                                        ,letterSpacing: 2,
+                                      ),),
+                                    SizedBox(
+                                      width: 50.0,
+                                    ),
+                                    Text('INSTRUMENT OVER RANGE \nMOVE BACK IMMEDIATELY',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 10.0,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                decoration: BoxDecoration(
+                                  borderRadius:BorderRadius.all(Radius.circular(40.0)),
+                                  color:     Colors.red,
+                                ),
+                                height: 35.0,
+                                width: 295.0,
+                              ),
+                              SizedBox(
+                                height: 80,
+                              ),
+                              // Second box
+                              Container(
+                                child: Row(
 
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              child: decorationBox()
-            ),
-            Container(
-              child: Row(
-                children:[
-                  Column(
+                                  children: [
+                                    SizedBox(
+                                      width: 15.0,
+                                    ),
+                                    Text('High',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold
+                                      ),),
+                                    SizedBox(
+                                      width: 90.0,
+                                    ),
+                                    Text('Limit Stay Times to less than\n 5 Hours',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 10.0,
+                                      ),),
 
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children:[
-                        intRadiationNumbers(1000),
-                        SizedBox(height: 75),
-                        intRadiationNumbers(100),
-                        SizedBox(height: 75),
-                        intRadiationNumbers(5),
-                        SizedBox(height: 75),
-                        doubleRadiationNumbers(0.05),
-                        SizedBox(height: 75),
-                        intRadiationNumbers(0),
-                      ]
-                  ),
-                  Align(
-                      alignment: Alignment(0, 0),
-                      child: radiationLevelBar()
+                                  ],
+                                ),
+                                decoration: BoxDecoration(
+                                  borderRadius:BorderRadius.all(Radius.circular(40.0)),
+                                  color:     Colors.red,
+                                ),
+                                height: 30.0,
+                                width: 295.0,
+                              ),
+
+                              SizedBox(
+                                height: 80,
+                              ),
+                              // Third box
+
+                              Container(
+                                child: Row(
+
+                                  children: [
+                                    SizedBox(
+                                      width: 15.0,
+                                    ),
+                                    Text('Medium',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold
+                                      ),),
+                                    SizedBox(
+                                      width: 70.0,
+                                    ),
+                                    Text('Turn Back Limit exceeded,\n REPORT IMMEDIATELY',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 10.0,
+                                      ),)
+                                  ],
+                                ),
+                                decoration: BoxDecoration(
+                                  borderRadius:BorderRadius.all(Radius.circular(40.0)),
+                                  color:     Colors.orange,
+                                ),
+                                height: 30.0,
+                                width: 295.0,
+                              ),
+
+                              SizedBox(
+                                height: 170,
+                              ),
+                              Container(
+                                child: Row(
+                                  children: [
+                                    SizedBox(
+                                      width: 15.0,
+                                    ),
+                                    Text('Background',
+
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold
+                                      ),),
+                                    SizedBox(
+                                      width: 15.0,
+                                    ),
+                                    Text('No Health Concern',
+
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 10.0,
+                                      ),)
+                                  ],
+                                ),
+                                decoration: BoxDecoration(
+                                    borderRadius:BorderRadius.all(Radius.circular(40.0)),
+                                    color:     Colors.green
+                                ),
+                                height: 30.0,
+                                width: 295.0,
+                              )
+                            ],
+                          )
+                      ),
+                    ]
                   )
-                ]
-              )
-            ),
-          ],
-        )
+
+                ),
+
+              ],
+            )
+          ),
+        ],
       ),
 
     );
@@ -56,28 +205,59 @@ class homePage extends StatelessWidget {
       children: [
         Container(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(
-                child: Text("Dose Rate"),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: 15.0,
+                  ),
+                  Container(
+                    child: Text("Dose Rate",
+                      style: TextStyle(
+                          fontSize: 20.0
+                          ,color: Colors.white
+                      ),
+                    ),
+                  ),
 
+                ],
               ),
-              SizedBox(
-                height: 60.0,
-              ),
-              Container(
-                child: Text("10mRad/hr"),
-
+              Spacer(),
+              Row(
+                // crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  SizedBox(
+                    width: 15.0,
+                  ),
+                  Container(
+                    child: Text(".002",
+                      style: TextStyle(
+                          fontSize: 40
+                          ,color: Colors.white
+                      ),
+                    ),
+                  ),
+                  Text("mRad/hr",
+                    style: TextStyle(
+                        fontSize: 20.0
+                        ,color: Colors.white
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-           width: 300,
+
+           width: 500,
           height: 100,
           margin: const EdgeInsets.all(12.0),
           decoration: new BoxDecoration(
 
-            color: Colors.redAccent,
-            borderRadius: BorderRadius.circular(10),
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(20),
             boxShadow: [
               BoxShadow(
                 color: Colors.black,
@@ -107,27 +287,32 @@ class homePage extends StatelessWidget {
   Widget radiationLevelBar()
   {
     return Container(
+
       child: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
             colors:[
-              Colors.red[900]!,
+
+              Colors.grey,
+              Colors.grey,
               Colors.red,
               Colors.orange,
               Colors.yellow,
               Colors.green
             ],
           ),
-          borderRadius:BorderRadius.all(Radius.circular(10.0)),
+          borderRadius:BorderRadius.all(Radius.circular(40.0)),
         )
       ),
-      height:400,
-      width:50,
+      height:480,
+      width:40,
       //color: Colors.deepPurpleAccent
     );
+
   }
+
   Widget intRadiationNumbers(int radiationLevel)
   {
     return Container(
